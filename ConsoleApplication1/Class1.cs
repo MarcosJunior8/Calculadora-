ebdc8750f60300e78f3ec0216e49a786bd3ca298
MarcosJunior8 <marcos.junior2@alunos.unis.edu.br>
2020-03-09 17:29:06 -0300

    Primeiro Commit

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        private List<double> listnumero = new List<double>();
+        
+
+            public void addnumero(double num)
+        {
+            this.listnumero.Add(num);
+        }
+
+        public double somar()
+        {
+            double resultado = 0;
+            foreach (double numero in this.listnumero)
+            {
+                resultado += numero;
+            }
+            this.listnumero.Clear();
+            return resultado;
+
+        }
+
+        public double subtrair()
+        {
+            double resultado = this.listnumero[0];
+            this.listnumero.RemoveAt(0);
+            foreach (double numero in this.listnumero)
+            {
+                resultado -= numero;
+            }
+            this.listnumero.Clear();
+            return resultado;
+
+        }
+
+        public double mult()
+        {
+            double resultado = this.listnumero[0];
+            this.listnumero.RemoveAt(0);
+            foreach (double numero in this.listnumero)
+            {
+                resultado *= numero;
+            }
+            this.listnumero.Clear();
+            return resultado;
+
+        }
+
+        public double divisao()
+        {
+            double resultado = this.listnumero[0];
+            this.listnumero.RemoveAt(0);
+            foreach (double numero in this.listnumero)
+            {
+                resultado /= numero;
+            }
+            this.listnumero.Clear();
+            return resultado;
+
+        }
+
+        public double media()
+        {
+            double resultado = 0;
+            double x = 0;
+            foreach(double numero in this.listnumero)
+            {
+                resultado += numero;
+                x = resultado / this.listnumero.Count;
+            }
+
+            return x;
+        }
+
+
+
+
+
+    }
+}
+
+    
+
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de numeros: ");
+            byte z = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Class1 c = new Class1();
+            for(int i = 1;i <= z; i++)
+            {
+                c.addnumero(Convert.ToDouble(Console.ReadLine()));
+        
+            }
+
+            Console.WriteLine("Somado: ");
+            System.Console.WriteLine("\n" + c.somar() + "\n");
+
+            for (int i = 1; i <= z; i++)
+            {
+                c.addnumero(Convert.ToDouble(Console.ReadLine()));
+
+            }
+            Console.WriteLine("Subtraido: ");
+            System.Console.WriteLine("\n" + c.subtrair() + "\n");
+
+            for (int i = 1; i <= z; i++)
+            {
+                c.addnumero(Convert.ToDouble(Console.ReadLine()));
+
+            }
+            Console.WriteLine("Mutiplicado: ");
+            System.Console.WriteLine("\n" + c.mult() + "\n");
+
+            for (int i = 1; i <= z; i++)
+            {
+                c.addnumero(Convert.ToDouble(Console.ReadLine()));
+
+            }
+            Console.WriteLine("Dividido: ");
+            System.Console.WriteLine("\n" + c.divisao() + "\n" );
+
+            for (int i = 1; i <= z; i++)
+            {
+                c.addnumero(Convert.ToDouble(Console.ReadLine()));
+
+            }
+            Console.WriteLine("Media aritmetica: ");
+            System.Console.WriteLine("\n" + c.media() + "\n");
+            Console.ReadKey();
+        }
+    }
+}
